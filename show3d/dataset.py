58082dfaac6d2d6eb745c12383c8c7c3d82e955c
Patrick Rim <pqrim@atlas.cs.yale.edu>
2026-07-31 01:10:29 -0400

    Added trained baseline.

--- show3d/dataset.py
+++ show3d/dataset.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-# pyre-strict
+# pyre-strict 
 
 """Generic dataloader helpers for the SHOW3D public starter repo.
 
